c85331b31a7d5139777a44346b2a2c9483b2f57e
Tim Graboski <tgraboski@tutela.com>
2025-05-29 14:20:11 -0700

    Operations in their own file

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,15 @@
+import { operations } from './operations';
 import { fullsize } from './utils';
 
 let bestValueSums;
 let bestResult;
 
-const Operations = [{
-  op: "+",
-  check: (n1, n2) => n1 > 0 && n2 > 0, // no sense in adding zero or negative numbers
-  apply: (n1, n2) => n1 + n2,
-  cost: 1
-}, {
-  op: "-",
-  check: (n1, n2) => n1 > n2 && n2 > 0, // no sense in subtracting zero or negative numbers
-  apply: (n1, n2) => n1 - n2,
-  cost: 1.05
-}, {
-  op: "_", // also subtract, but backwards for when left is smaller than right
-  check: (n2, n1) => n1 > n2 && n2 > 0,
-  apply: (n2, n1) => n1 - n2,
-  cost: 1.05
-}, {
-  op: "*",
-  check: (n1, n2) => n1 > 1 && n2 > 1, // no sense in multiplying by zero or one
-  apply: (n1, n2) => n1 * n2,
-  cost: 1.2
-}, {
-  op: "/",
-  check: (n1, n2) => n2 > 1 && n1 % n2 === 0, // no sense in dividing by zero or one, and only whole numbers
-  apply: (n1, n2) => n1 / n2,
-  cost: 1.3
-}, {
-  op: "?", // also divide, but backwards for when left is smaller than right
-  check: (n2, n1) => n2 > 1 && n1 % n2 === 0,
-  apply: (n2, n1) => n1 / n2,
-  cost: 1.3
-}];
-
-const ALREADY_USED = -1;
 const _recurse_solve_numbers = (
   numbers,
-  alreadyUsed,
-  previousI,
+  alreadyUsed: boolean[],
+  previousI: number,
   wasGenerated,
-  target,
+  target: number,
   levels,
   valueSums
 ) => {
@@ -62,7 +30,7 @@ const _recurse_solve_numbers = (
         continue;
       }
 
-      for (const operation of Operations) {
+      for (const operation of operations) {
         if (!operation.check(left[0], right[0])) {
           continue;
         }
