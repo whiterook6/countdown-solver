b1669889038559ff818fc6d319fbaed040723bed
Tim Graboski <whiterook6@gmail.com>
2025-05-28 23:20:38 -0700

    found a bug where it won't solve 3*5 + 7*11 = 92

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+const shuffle = (array) => {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    const randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+}
+
+
 const solveRecursively = (target, currentValue, numbers, expression) => {
   if (currentValue === target) {
     return expression;
@@ -11,6 +28,13 @@ const solveRecursively = (target, currentValue, numbers, expression) => {
     const number = numbers[i];
     const newNumbers = numbers.slice();
     newNumbers.splice(i, 1);
+
+    if (currentValue === 0){
+      const solved = solveRecursively(target, number, newNumbers, `${number}`);
+      if (solved){
+        return solved;
+      }
+    }
 
     const add = solveRecursively(target, currentValue + number, newNumbers, `(${expression} + ${number})`);
     if (add) {
@@ -53,7 +77,9 @@ const solveRecursively = (target, currentValue, numbers, expression) => {
 }
 
 const solve = (target, numbers) => {
-  console.log(solveRecursively(target, 0, numbers, '0'));
+  const shuffledNumbers = [...numbers];
+  shuffle(shuffledNumbers);
+  console.log(solveRecursively(target, 0, shuffledNumbers, '0'));
 };
 
-solve(228, [100, 8, 5, 10, 6, 9]);
+solve(92, [3, 5, 7, 11]);
